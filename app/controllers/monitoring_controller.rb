12ff9e0c8e7fab8d731020fd2083b7704093e941
Максим Петров <maksim.petrov@kodep.ru>
2015-07-14 12:02:05 +0300

    update

--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -3,28 +3,20 @@ class MonitoringController < ApplicationController
   
   def index
   	@machines_all = $redis.smembers("machines")
+  	@machines_parameters = set_machine_parameters
   end
 
   def machines_statistic
-  	
-  	$redis.sadd("machines", "#{params[:hostname]}")
-  	
-  	$redis.hset("status", "#{params[:hostname]}", "#{params[:status]}")
- 	$redis.expire("status", 10)
-  
-  	$redis.hset("cores", "#{params[:hostname]}", "#{params[:cores]}")
-  	$redis.expire("cores", 10)
-
-	$redis.hset("available_memory", "#{params[:hostname]}", "#{params[:available_memory]}")
-  	$redis.expire("available_memory", 10)
-
-  	$redis.hset("load_average", "#{params[:hostname]}", "#{params[:load_average]}")
-  	$redis.expire("load_average", 10)
-
-	$redis.hset("memory_usage", "#{params[:hostname]}", "#{params[:memory_usage]}")
-  	$redis.expire("memory_usage", 10)
-  
+  	$redis.sadd("machines", params[:hostname])
+  	set_machine_parameters.each do |parameter|
+  	 $redis.hset(parameter, params[:hostname], params[parameter.to_sym])
+ 	 $redis.expire("status", 10)
+ 	end 
     render nothing: true
+  end
+
+  def set_machine_parameters
+  	machines_parameters = ["cores", "available_memory", "load_average", "memory_usage", "status"]
   end
 
 end
